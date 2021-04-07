5c125d003d7b52787686c1b818389b1bc7943c2d
onno204 <15333546+onno204@users.noreply.github.com>
2021-04-07 23:49:01 +0200

    Added tablet base

---
+++ meos/__resource.lua
@@ -0,0 +1,16 @@
+--Gemaakt door PerdonIT
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'main.lua'
+} 
+
+
+files {
+    'html/index.html',
+    'html/style.css',
+    "html/script.js",
+    "html/tablet.png"
+}
---
+++ meos/main.lua
@@ -0,0 +1,87 @@
+ guiEnabled = false
+Citizen.CreateThread(function()
+  while true do
+      if guiEnabled then
+          DisableControlAction(0, 1, guiEnabled) -- LookLeftRight
+          DisableControlAction(0, 2, guiEnabled) -- LookUpDown
+
+          DisableControlAction(0, 142, guiEnabled) -- MeleeAttackAlternate
+
+          DisableControlAction(0, 106, guiEnabled) -- VehicleMouseControlOverride
+
+      end
+      Citizen.Wait(0) --MH LUA
+  end
+end)
+
+function PrintChatMessage(text)
+  TriggerEvent('chatMessage', "system", { 255, 0, 0 }, text)
+end
+  
+RegisterNUICallback('NUIFocusOff', function()
+  
+  Gui(false)
+end)
+
+
+
+RegisterCommand("meos", function(source, args, rawCommand)
+if guiEnabled then
+Gui(false)
+else
+Gui(true)
+end
+end, false)
+
+
+
+    function Gui(toggle)
+      SetNuiFocus(toggle, toggle)
+      guiEnabled = toggle
+      if guiEnabled then 
+        startAnim()
+      else
+        stopAnim()
+      end
+      SendNUIMessage({
+          type = "enableui",
+          enable = toggle
+      })
+   end
+
+
+  AddEventHandler('onClientResourceStart', function(resourceName)
+    if(GetCurrentResourceName() ~= resourceName) then
+      return
+    end
+    Gui(false)
+end)
+
+function startAnim()
+	Citizen.CreateThread(function()
+	  RequestAnimDict("amb@world_human_seat_wall_tablet@female@base")
+	  while not HasAnimDictLoaded("amb@world_human_seat_wall_tablet@female@base") do
+	    Citizen.Wait(0)
+	  end
+		attachObject()
+		TaskPlayAnim(GetPlayerPed(-1), "amb@world_human_seat_wall_tablet@female@base", "base" ,8.0, -8.0, -1, 50, 0, false, false, false)
+	end)
+end
+
+function attachObject()
+	tab = CreateObject(GetHashKey("prop_cs_tablet"), 0, 0, 0, true, true, true)
+	AttachEntityToEntity(tab, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 57005), 0.17, 0.10, -0.13, 20.0, 180.0, 180.0, true, true, false, true, 1, true)
+end
+
+function stopAnim()
+	StopAnimTask(GetPlayerPed(-1), "amb@world_human_seat_wall_tablet@female@base", "base" ,8.0, -8.0, -1, 50, 0, false, false, false)
+	DeleteEntity(tab)
+end
+
+
+
+
+--RegisterNetEvent("output")
+--AddEventHandler("output", function(argument)
+  --  TriggerEvent("chatMessage", "[Success]", {0,255,0}, argument)
+--end)
