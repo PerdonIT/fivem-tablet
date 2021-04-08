f75986013702c32239a0172b8239a9936da2efe1
onno204 <15333546+onno204@users.noreply.github.com>
2021-04-08 14:25:27 +0200

    Fixed code formatting

--- html/script.js
+++ html/script.js
@@ -1,64 +1,60 @@
-$(function() {
-    window.addEventListener('message', function(event) {
+$(function () {
+    window.addEventListener('message', function (event) {
         if (event.data.type == "enableui") {
             document.body.style.display = event.data.enable ? "block" : "none";
-		} 
-		else if (event.data.type == "backHome") {
+        } else if (event.data.type == "backHome") {
             document.body.style.display = "block";
-		
-		}
+
+        }
     });
 
     document.onkeyup = function (data) {
         if (data.which == 27) { // Escape toets
             $.post('http://meos/NUIFocusOff', JSON.stringify({}));
-            
+
         }
     };
-	
-	
- 
 
 
-	function backHome() {
-		document.body.style.display = "block";
-	};
+    function backHome() {
+        document.body.style.display = "block";
+    }
 
-	
-	dragElement(document.getElementById("tablet"));
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    elmnt.onmousedown = dragMouseDown;
-  }
+    dragElement(document.getElementById("tablet"));
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById(elmnt.id + "header")) {
+            document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+        } else {
+            elmnt.onmousedown = dragMouseDown;
+        }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
 
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-	
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
 });
